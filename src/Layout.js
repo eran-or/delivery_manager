b2073e7f62e2dbf611c0912f4473bf05a3948744
eranor <erano@welldone-software.com>
2018-03-20 13:03:35 +0200

    clean

---
+++ src/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className="lauout">
+        wellcome
+      </div>
+    );
+  }
+}
+
+export default Layout;
