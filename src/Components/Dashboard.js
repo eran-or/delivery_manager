c3aa37bc7bc469a15ee3b89aabef18e77a77560f
eranor <erano@welldone-software.com>
2018-03-21 09:38:32 +0200

    alignment

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -7,8 +7,14 @@ import OrdersMap from './GoogleMap'
 
 const Dashboard = (props) => {
   return (
-    <div>
-      <div className="dashboardTop">
+    <div className="container d-flex justify-content-center flex-column align-items-center">
+      <div className="dashboardTop row align-items-center">
+        <div className="card mb-4 box-shadow">
+          <div className="card-body"></div>
+        </div>
+        <div className="card mb-4 box-shadow">
+          <div className="card-body"></div>
+        </div>
         <div className="card mb-4 box-shadow">
           <div className="card-body"></div>
         </div>
